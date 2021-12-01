9c4dba9321b1bfa69914d5f2cdb78ef4d2a570d8
rutraq <a3332133@gmail.com>
2021-12-01 21:43:20 +0100

    get text request

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ def hello_world():
     return jsonify(test)
 
 
-@app.route('/name', methods=['GET'])
-def add_to_database():
-    test = {"user": "debil"}
-    return jsonify(test)
+@app.route('/get-text')
+def get_from_database():
+    conn = psycopg2.connect(
+        "dbname='slihduor' user='slihduor' host='rajje.db.elephantsql.com' password='Z3kDd9k-Hzri0TsNeXOEKYkb7jo9wClC'")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM website_text WHERE page='{0}'".format(request.args.get("page")))
+    row = cur.fetchall()
+    text = {}
+    for r in row:
+        text[r[0]] = r[2]
+    return jsonify(text)
 
 
 if __name__ == '__main__':
