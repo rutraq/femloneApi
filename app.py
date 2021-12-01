5f793782fde9f29e89d0ef7fce8f0210cf4c5cc8
rutraq <a3332133@gmail.com>
2021-12-01 11:42:55 +0100

    second api request

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,12 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
+    test = {"user": "debil"}
+    return jsonify(test)
+
+
+@app.route('/name', methods=['GET'])
+def add_to_database():
     test = {"user": "debil"}
     return jsonify(test)
 
