631b09760a250624ae10c5d326abf6df74b21717
rutraq <a3332133@gmail.com>
2021-12-02 19:31:33 +0100

    login and mongo db

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import psycopg2
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
-def hello_world():
-    test = {"user": "debil"}
-    return jsonify(test)
-
-
 @app.route('/get-text')
 def get_from_database():
-    conn = psycopg2.connect(
-        "dbname='slihduor' user='slihduor' host='rajje.db.elephantsql.com' password='Z3kDd9k-Hzri0TsNeXOEKYkb7jo9wClC'")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM website_text WHERE page='{0}'".format(request.args.get("page")))
-    row = cur.fetchall()
-    text = {}
-    for r in row:
-        text[r[0]] = r[2]
-    return jsonify(text)
+    mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/text")
+    db = mongodb_client.db
+    if request.args.get("page") == "about":
+        texts = db.about.find()
+    elif request.args.get("page") == "home":
+        texts = db.home.find()
+    else:
+        texts = db.about.find()
+    data = {}
+    for text in texts:
+        data[text['id']] = text['text']
+    return jsonify(data)
+
+
+@app.route('/admin')
+def login():
+    mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/admin")
+    db = mongodb_client.db
+    user = db.users.find_one({"login": request.args.get("login")})
+    if user is not None:
+        if request.args.get("password") == user['password']:
+            return jsonify(message="good")
+        else:
+            return jsonify(message="bad")
+    else:
+        return jsonify(message="bad")
 
 
 if __name__ == '__main__':
