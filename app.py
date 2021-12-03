bc0c427de982aa01ba9a1dca56ee46b0001e39b0
rutraq <a3332133@gmail.com>
2021-12-03 13:20:23 +0100

    generating of jwt token

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+import python_jwt
+from jwcrypto.jws import InvalidJWSSignature
+from werkzeug.security import generate_password_hash, check_password_hash
+import python_jwt as jwt, jwcrypto.jwk as jwk, datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -28,12 +32,29 @@ def login():
     db = mongodb_client.db
     user = db.users.find_one({"login": request.args.get("login")})
     if user is not None:
-        if request.args.get("password") == user['password']:
-            return jsonify(message="good")
+        if check_password_hash(user["password"], request.args.get("password")):
+            return jwt_test(user["login"], user["password"])
         else:
             return jsonify(message="bad")
     else:
         return jsonify(message="bad")
+
+
+def jwt_test(name, password):
+    key = jwk.JWK.generate(kty='RSA', size=2048)
+    key2 = jwk.JWK.generate(kty='RSA', size=1024)
+    payload = {
+        'login': name,
+        'password': password}
+    token = jwt.generate_jwt(payload, key, 'PS256', datetime.timedelta(minutes=60))
+    token2 = jwt.generate_jwt(payload, key2, 'PS256', datetime.timedelta(minutes=60))
+    try:
+        jwt.verify_jwt(token2, key, ['PS256'])
+    except InvalidJWSSignature:
+        print("Что ты за токен отправил еблан?")
+    response = make_response(jsonify(message="good"))
+    response.headers["token"] = token
+    return response
 
 
 if __name__ == '__main__':
