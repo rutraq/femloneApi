e7c68ffea0191da066bd2fb60ade436e77006938
rutraq <a3332133@gmail.com>
2021-12-05 14:05:13 +0100

    jwt new library

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import python_jwt as jwt
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-import python_jwt
-from jwcrypto.jws import InvalidJWSSignature
-from werkzeug.security import generate_password_hash, check_password_hash
-import python_jwt as jwt, jwcrypto.jwk as jwk, datetime
+from werkzeug.security import check_password_hash
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -33,28 +33,51 @@ def login():
     user = db.users.find_one({"login": request.args.get("login")})
     if user is not None:
         if check_password_hash(user["password"], request.args.get("password")):
-            return jwt_test(user["login"], user["password"])
+            return jwt_generate(user['login'], request.args.get("password"), user["password"])
         else:
             return jsonify(message="bad")
     else:
         return jsonify(message="bad")
 
 
-def jwt_test(name, password):
-    key = jwk.JWK.generate(kty='RSA', size=2048)
-    key2 = jwk.JWK.generate(kty='RSA', size=1024)
+# def jwt_test(name, password):
+#     key = jwk.JWK.generate(kty='RSA', size=2048)
+#     print(key)
+#     payload = {
+#         'login': name,
+#         'password': password}
+#     token = jwt.generate_jwt(payload, key.export_private(), 'RS256', datetime.timedelta(minutes=60))
+#     try:
+#         jwt.verify_jwt(token, key.export_public(), ['RS256'])
+#     except InvalidJWSSignature:
+#         print("Что ты за токен отправил еблан?")
+#     response = make_response(jsonify(message="good"))
+#     response.headers["token"] = token
+#     return response
+
+def jwt_generate(name, password, password_key):
     payload = {
-        'login': name,
-        'password': password}
-    token = jwt.generate_jwt(payload, key, 'PS256', datetime.timedelta(minutes=60))
-    token2 = jwt.generate_jwt(payload, key2, 'PS256', datetime.timedelta(minutes=60))
-    try:
-        jwt.verify_jwt(token2, key, ['PS256'])
-    except InvalidJWSSignature:
-        print("Что ты за токен отправил еблан?")
+        "login": name,
+        "password": password,
+        "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(minutes=30)
+    }
+    token = jwt.encode(payload, password_key, algorithm="HS256")
     response = make_response(jsonify(message="good"))
-    response.headers["token"] = token
+    response.set_cookie("token", token)
     return response
+
+
+@app.route("/check")
+def ch():
+    return jsonify(check=jwt_check(request.cookies.get("token"), ))
+
+
+def jwt_check(token, password_key):
+    try:
+        jwt.decode(token, password_key, algorithms=["HS256"])
+        return True
+    except jwt.exceptions.InvalidSignatureError:
+        return False
 
 
 if __name__ == '__main__':
