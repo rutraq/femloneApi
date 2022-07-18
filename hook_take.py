9017091526830987acce208bfe05e1fd67025414
Phanaty <evgeniy.orlov19901@gmail.com>
2022-07-18 16:14:41 +0300

    add multi take

--- hook_take.py
+++ hook_take.py
@@ -26,39 +26,55 @@ def make_order():
 class ByBit:
     def __init__(self, hook):
         self.position = hook["Position"]
-        self.multi_take = hook["Multi Take"]
-        self.symbol = hook["Symbol"]
-        self.order = hook["Order"]
-        self.entry_price = hook["Entry Price"]
-        self.stop_loss_price = hook["Stop Loss Price"]
-        self.stop_loss_two_price = hook["Stop Loss Two Price"]
-        self.take_profit_price = hook["Take Profit Price"]
-        self.take_profit_two_price = hook["Take Profit Two Price"]
-        self.take_profit_three_price = hook["Take Profit Three Price"]
-        self.leverage = hook["Leverage"]
-        self.close_volume_first = hook["Close Volume First"]
-        self.close_volume_second = hook["Close Volume Second"]
-        self.close_volume_three = hook["Close Volume Three"]
-        self.percent_balance = hook["Percent Balance"]
-        self.api_key = hook["Api Key"]
-        self.secret_api_key = hook["Secret Api Key"]
-        self.position_mode = hook["Position mode"]
-        self.margin_mode = hook["Margin mode"]
-        self.position_tp_mode = hook["Position TP/SL mode"]
-        self.position_idx = 0
-        self.session_auth = usdt_perpetual.HTTP(endpoint="https://api-testnet.bybit.com", api_key=self.api_key,
+        if self.position == "Open Short Position" or self.position == "Open Long Position":
+            self.multi_take = hook["Multi Take"]
+            self.symbol = hook["Symbol"]
+            self.order = hook["Order"]
+            self.entry_price = hook["Entry Price"]
+            self.stop_loss_price = hook["Stop Loss Price"]
+            self.stop_loss_two_price = hook["Stop Loss Two Price"]
+            self.take_profit_price = hook["Take Profit Price"]
+            self.take_profit_two_price = hook["Take Profit Two Price"]
+            self.take_profit_three_price = hook["Take Profit Three Price"]
+            self.leverage = hook["Leverage"]
+            self.close_volume_first = hook["Close Volume First"]
+            self.close_volume_second = hook["Close Volume Second"]
+            self.close_volume_three = hook["Close Volume Three"]
+            self.percent_balance = hook["Percent Balance"]
+            self.api_key = hook["Api Key"]
+            self.secret_api_key = hook["Secret Api Key"]
+            self.position_mode = hook["Position mode"]
+            self.margin_mode = hook["Margin mode"]
+            self.position_tp_mode = hook["Position TP/SL mode"]
+            self.position_idx = 0
+            self.session_auth = usdt_perpetual.HTTP(endpoint="https://api-testnet.bybit.com", api_key=self.api_key,
                                                 api_secret=self.secret_api_key)
 
         try:
             self.set_position_mode()
-            self.set_margin_mode()
-            self.set_leverage()
-            self.set_position_tp_sl_mode()
         except exceptions.InvalidRequestError:
             telebot.TeleBot("5392822083:AAHSdKNl_C60QjyVn0vqYv6jIln6rV2MG9Y").send_message("-699678335",
                                                                                            "Артур дурак? да или да")
-        self.open_trade()
+            try:
+                self.set_margin_mode()
+            except exceptions.InvalidRequestError:
+                telebot.TeleBot("5392822083:AAHSdKNl_C60QjyVn0vqYv6jIln6rV2MG9Y").send_message("-699678335",
+                                                                                               "Артур дурак? да или да")
+            try:
+                self.set_position_tp_sl_mode()
+            except exceptions.InvalidRequestError:
+                telebot.TeleBot("5392822083:AAHSdKNl_C60QjyVn0vqYv6jIln6rV2MG9Y").send_message("-699678335",
+                                                                                               "Артур дурак? да или да")
+            try:
+                self.set_leverage()
+            except exceptions.InvalidRequestError:
+                telebot.TeleBot("5392822083:AAHSdKNl_C60QjyVn0vqYv6jIln6rV2MG9Y").send_message("-699678335",
+                                                                                               "Артур дурак? да или да")
 
+        if self.position == "Open Short Position" or self.position == "Open Long Position":
+            self.open_trade()
+
+    # установка плеча
     def set_leverage(self):
         self.session_auth.set_leverage(
             symbol=self.symbol,
@@ -66,6 +82,7 @@ class ByBit:
             sell_leverage=self.leverage
         )
 
+    # установка режима позиции
     def set_position_mode(self):
         if self.position_mode == "One-Way Mode":
             self.session_auth.position_mode_switch(
@@ -79,8 +96,13 @@ class ByBit:
                 symbol=self.symbol,
                 mode="BothSide"
             )
-            self.position_idx = 1
+            if self.order == "Buy":
+                self.position_idx = 1
 
+            elif self.order == "Sell":
+                self.position_idx = 2
+
+    # установка режима маржи
     def set_margin_mode(self):
         if self.margin_mode == "Isolated Mode":
             self.session_auth.cross_isolated_margin_switch(
@@ -97,6 +119,7 @@ class ByBit:
                 sell_leverage=self.leverage
             )
 
+    # Настройка TP/SL
     def set_position_tp_sl_mode(self):
         if self.position_tp_mode == "Full Mode":
             self.session_auth.full_partial_position_tp_sl_switch(
@@ -117,8 +140,9 @@ class ByBit:
         order_size = float(order_balance) / float(last_price)
         return order_size
 
+    # открытие позиции
     def open_trade(self):
-        qty_order = round(self.balance_volume(), 3)
+        qty_order = round(self.balance_volume(), 1)
         self.session_auth.place_active_order(
             symbol=self.symbol,
             side=self.order,
@@ -129,8 +153,12 @@ class ByBit:
             close_on_trigger=False,
             position_idx=self.position_idx
         )
-        self.tp_sl_profit(qty_order)
+        if self.multi_take == "false":
+            self.tp_sl_profit(qty_order)
+        if self.multi_take == "true":
+            self.tp_sl_profit_multi(qty_order)
 
+    # установка тейк профита и стоп лосса
     def tp_sl_profit(self, qty_order):
         self.session_auth.set_trading_stop(
             symbol=self.symbol,
@@ -141,6 +169,42 @@ class ByBit:
             sl_trigger_by="LastPrice",
             tp_size=qty_order,
             sl_size=qty_order,
+            position_idx=self.position_idx
+        )
+
+    # установка тейк профита и стоп лосса для мультитейка
+    def tp_sl_profit_multi(self, qty_order):
+        first_take_volume = float(qty_order) * (int(self.close_volume_first) / 100)
+        second_take_volume = float(qty_order) * (int(self.close_volume_second) / 100)
+        three_take_volume = float(qty_order) - first_take_volume - second_take_volume
+
+        # первый тейк профит
+        self.session_auth.set_trading_stop(
+            symbol=self.symbol,
+            side=self.order,
+            take_profit=float(self.take_profit_price),
+            tp_trigger_by="LastPrice",
+            tp_size=round(first_take_volume, 1),
+            position_idx=self.position_idx
+        )
+
+        # второй тейк профит
+        self.session_auth.set_trading_stop(
+            symbol=self.symbol,
+            side=self.order,
+            take_profit=float(self.take_profit_two_price),
+            tp_trigger_by="LastPrice",
+            tp_size=round(second_take_volume, 1),
+            position_idx=self.position_idx
+        )
+
+        # третий тейк профит
+        self.session_auth.set_trading_stop(
+            symbol=self.symbol,
+            side=self.order,
+            take_profit=float(self.take_profit_three_price),
+            tp_trigger_by="LastPrice",
+            tp_size=round(three_take_volume, 1),
             position_idx=self.position_idx
         )
 
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from pybit import usdt_perpetual
 
-hook = {"Position": "Open Short Position", "Multi Take": "true", "Symbol": "BTCUSDT", "Order": "Sell",
+hook = {"Position": "Open Short Position", "Multi Take": "true", "Symbol": "AVAXUSDT", "Order": "Sell",
         "Entry Price": "21800", "Stop Loss Price": "22000", "Stop Loss Two Price": "21000.55",
         "Take Profit Price": "20000.44", "Take Profit Two Price": "19000",
-        "Take Profit Three Price": "18000", "Leverage": "10", "Close Volume First": "20",
+        "Take Profit Three Price": "18000", "Leverage": "20", "Close Volume First": "20",
         "Close Volume Second": "50", "Close Volume Three": "100", "Percent Balance": "2.5", "Api Key": "CRQIAjML2aykFyjnAF", "Secret Api Key": "G0wIXLbDKrEYKWBuU9csBp0uRR8hqX2hU0G1"}
 
 position = hook["Position"]
@@ -32,54 +32,8 @@ session_auth = usdt_perpetual.HTTP(
 )
 
 
-def set_leverage(symbol, leverage):
-        session_auth.cross_isolated_margin_switch(
-                symbol=symbol,
-                buy_leverage=leverage,
-                sell_leverage=leverage
-        )
 
-
-# получаем данные о балансе и вычисляем объем ордера
-def balance_volume(symbol, percent_balance, leverage):
-        wallet_balance = session_auth.get_wallet_balance(coin="USDT")['result']['USDT']['equity']
-        last_price = session_auth.orderbook(symbol=symbol)['result'][0]['price']
-        order_balance = float(wallet_balance) * (float(percent_balance)/100) * int(leverage)
-        order_size = float(order_balance) / float(last_price)
-        print(order_size)
-        return order_size
-
-
-# открываем позицию
-def open_trade(symbol, order, percent_balance, leverage, take_profit_price, stop_loss_price):
-        qty_order = round(balance_volume(symbol, percent_balance, leverage), 3)
-        session_auth.place_active_order(
-                symbol=symbol,
-                side=order,
-                order_type="Market",
-                qty=qty_order,
-                time_in_force="GoodTillCancel",
-                reduce_only=False,
-                close_on_trigger=False,
-                position_idx=0
-        )
-        tp_sl_profit(symbol, order, take_profit_price, stop_loss_price, qty_order)
-
-
-# установка тейк профита и стоп лосса
-def tp_sl_profit(symbol, order, take_profit_price, stop_loss_price, qty_order):
-        session_auth.set_trading_stop(
+session_auth.full_partial_position_tp_sl_switch(
             symbol=symbol,
-            side=order,
-            take_profit=float(take_profit_price),
-            stop_loss=float(stop_loss_price),
-            tp_trigger_by="LastPrice",
-            sl_trigger_by="LastPrice",
-            tp_size=qty_order,
-            sl_size=qty_order,
-            position_idx=0
-        )
-
-
-set_leverage(symbol, leverage)
-open_trade(symbol, order, percent_balance, leverage, take_profit_price, stop_loss_price)
+            tp_sl_mode="Partial"
+            )
