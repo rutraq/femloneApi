27f2e98a82a5967741fa4bc777dba0ab088dc654
rutraq <a3332133@gmail.com>
2022-07-24 13:30:49 +0200

    make registration functions in telegram bot

--- telegramBot.py
+++ telegramBot.py
@@ -3,6 +3,8 @@ import telebot
 from telebot import types
 import pybit
 import os
+from datetime import datetime
+import psycopg2
 
 
 class TelegramBot:
@@ -12,30 +14,91 @@ class TelegramBot:
         self.group_id = "-699678335"
         self.session = pybit.HTTP("https://api.bybit.com",
                                   api_key="oMF5d6V3kf4DQOcX20", api_secret="Zv9FrzWpKk39CSUMVmZlGDqkEHfAE1HboLmz")
+        self.conn = psycopg2.connect(
+            "dbname='slihduor' user='slihduor' host='rajje.db.elephantsql.com' password='Z3kDd9k-Hzri0TsNeXOEKYkb7jo9wClC'")
+        self.cursor = self.conn.cursor()
 
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        command1 = types.KeyboardButton('Баланс')
-        command2 = types.KeyboardButton('Пидор ли женя?')
-        command3 = types.KeyboardButton('Открытые позиции')
-        command4 = types.KeyboardButton('Ошибки')
-        command5 = types.KeyboardButton('Unrealised pnl')
-        markup.add(command1, command2, command3, command4, command5)
-        self.bot.send_message(self.group_id, "Выберите команду:", reply_markup=markup)
+        @self.bot.message_handler(commands=['start'])
+        def start(message):
+            self.bot.send_message(message.chat.id, "Введите код который вам был выдан.")
 
         @self.bot.message_handler(content_types=['text'])
         def get_text_messages(message):
             if message.text == "Пидор ли женя?":
-                self.bot.send_message(self.group_id, "Конечно да!")
+                if self.check_user(message.chat.id):
+                    self.bot.send_message(self.group_id, "Конечно да!")
             elif message.text == "Открытые позиции":
-                self.get_positions()
+                if self.check_user(message.chat.id):
+                    self.get_positions()
             elif message.text == "Баланс":
-                self.get_balance()
+                if self.check_user(message.chat.id):
+                    self.get_balance()
             elif message.text == "Ошибки":
-                self.get_errors()
+                if self.check_user(message.chat.id):
+                    self.get_errors()
             elif message.text == "Unrealised pnl":
-                self.unrealised_pnl()
+                if self.check_user(message.chat.id):
+                    self.unrealised_pnl()
+            elif message.text == "Дата окончания подписки":
+                if self.check_user(message.chat.id):
+                    self.check_date_of_subscription(message.chat.id)
+            else:
+                try:
+                    if 99999 < int(message.text) < 1000000:
+                        self.cursor.execute(
+                            "update payments set user_id = {0}, payment_number = 0 where payment_number = {1}".format(
+                                int(message.chat.id), int(message.text)))
+                        self.conn.commit()
+                        self.bot.send_message(message.chat.id, "Ваша учётная запись успешно привязана")
+
+                        self.send_reply_markup(self.group_id)
+                    else:
+                        self.bot.send_message(message.chat.id, "Код хуйня")
+                except ValueError:
+                    pass
+
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def call_back_handler(message):
+            user_id = message.message.chat.id
+            if message.data == "pidor":
+                if self.check_user(user_id):
+                    self.bot.send_message(self.group_id, "Конечно да!")
+                    self.send_reply_markup(user_id)
+            elif message.data == "positions":
+                if self.check_user(user_id):
+                    self.get_positions()
+                    self.send_reply_markup(user_id)
+            elif message.data == "balance":
+                if self.check_user(user_id):
+                    self.get_balance()
+                    self.send_reply_markup(user_id)
+            elif message.data == "errors":
+                if self.check_user(user_id):
+                    self.get_errors()
+                    self.send_reply_markup(user_id)
+            elif message.data == "pnl":
+                if self.check_user(user_id):
+                    self.unrealised_pnl()
+                    self.send_reply_markup(user_id)
+            elif message.data == "date":
+                if self.check_user(user_id):
+                    self.check_date_of_subscription(user_id)
+                    self.send_reply_markup(user_id)
+            self.bot.answer_callback_query(message.id, "")
 
         self.bot.infinity_polling()
+
+    def send_reply_markup(self, user_id):
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        # markup = types.ReplyKeyboardMarkup(row_width=2)
+        command1 = types.InlineKeyboardButton('Баланс', callback_data="balance")
+        command2 = types.InlineKeyboardButton('Пидор ли женя?', callback_data="pidor")
+        command3 = types.InlineKeyboardButton('Открытые позиции', callback_data="positions")
+        command4 = types.InlineKeyboardButton('Ошибки', callback_data="errors")
+        command5 = types.InlineKeyboardButton('Unrealised pnl', callback_data="pnl")
+        command6 = types.InlineKeyboardButton('Дата окончания подписки', callback_data="date")
+        markup.add(command1, command2, command3, command4, command5, command6)
+        self.bot.send_message(user_id, "Выберите команду:", reply_markup=markup)
 
     def get_balance(self):
         balance = round(self.session.get_wallet_balance(coin="USDT")['result']['USDT']['equity'], 2)
@@ -71,6 +134,24 @@ class TelegramBot:
     def unrealised_pnl(self):
         balance = round(self.session.get_wallet_balance(coin="USDT")['result']['USDT']['unrealised_pnl'], 2)
         self.bot.send_message(self.group_id, "Ваш unrealised pnl USDT: {0}".format(balance))
+
+    def check_user(self, user_id):
+        self.cursor.execute("select * from payments where user_id = {0}".format(user_id))
+        ans = self.cursor.fetchall()
+        if len(ans) == 0:
+            return False  # пользователь отсутсвует
+        else:
+            date = ans[0][2]
+            date_now = datetime.now().date()
+            if date_now < date:
+                return True  # подписка ещё действительна
+            else:
+                self.bot.send_message(user_id, "Действие вашей подписки закончилось")
+                return False  # подписка зкончилась
+
+    def check_date_of_subscription(self, user_id):
+        self.cursor.execute("select expiry_date from payments where user_id = {0}".format(user_id))
+        self.bot.send_message(user_id, "Дата окончания вашей подписки: {0}".format(self.cursor.fetchall()[0][0]))
 
 
 if __name__ == "__main__":
