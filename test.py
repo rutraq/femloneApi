27a1f851a60685b2937572ff4f6fd10f4ab7a05b
Phanaty <evgeniy.orlov19901@gmail.com>
2022-08-02 12:09:08 +0300

    new version open trade and cancel stop loss

--- test.py
+++ test.py
@@ -8,3 +8,16 @@ session_auth = usdt_perpetual.HTTP(
 )
 
 
+print(session_auth.place_conditional_order(
+    symbol="BTCUSDT",
+    order_type="Market",
+    side="Sell",
+    qty=0.015,
+    base_price=21000,
+    stop_px=20800.8,
+    time_in_force="GoodTillCancel",
+    trigger_by="LastPrice",
+    reduce_only=False,
+    close_on_trigger=False,
+    position_idx=0
+))
